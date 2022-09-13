9f729b239c74ed7cd217fcfeb5aff75b7567bca3
강철원 <cjfdnjs158@naver.com>
2022-09-13 20:57:21 +0900

    bug fix2

--- src/client/js/menubarLeft.js
+++ src/client/js/menubarLeft.js
@@ -1,8 +1,10 @@
 const menubarLeftBtn = document.querySelector(".menubar-left__Btn");
 const menubarLeftWrapper = document.querySelector(".menubar-left__wrapper");
+const menubarLeftIcon = document.querySelectorAll(".icon");
 
 const handleMenubar = () => {
   menubarLeftWrapper.classList.toggle("menubar-left__wide");
+  menubarLeftIcon.forEach((x) => x.classList.toggle("icon__wide"));
 };
 
 menubarLeftBtn.addEventListener("click", handleMenubar);
